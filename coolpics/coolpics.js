2e0d55c8a3d1d679fe0919b735d495fd7d3f9b9d
AnhMeo <u799696294@byui.edu>
2024-10-11 12:53:05 +0700

    added menu javascript

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,18 @@
 const menuToggler = document.querySelector('button')
 
 function toggleMenu () {
-    
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('hide');
 }
+
+function handleResize () {
+    const menu = document.querySelector('.menu');
+    if (window.innerWidth > 1000){
+        menu.classList.remove('hide');
+    }   else {
+        menu.classList.add('hide');
+    }
+}
+
+menuToggler.addEventListener('click', toggleMenu);
+window.addEventListener('resize', handleResize);
